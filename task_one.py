0a3e79cb1004933ee28723218fde5480c198ae4d
Matthew Kendall <47122515+mattkendall11@users.noreply.github.com>
2024-10-15 19:23:49 +0100

    task three complete

--- task_one.py
+++ task_one.py
@@ -1,7 +1,8 @@
 import time
+
+import numpy as np
 from tqdm.auto import tqdm
 import matplotlib.pyplot as plt
-
 from utils.matrix import matrix_circuit
 from utils.tensor import tensor_circuit
 
@@ -12,21 +13,37 @@ x = list(range(2,25))
 matrix_runtimes = []
 tensor_runtimes = []
 for qubit in tqdm(x):
-
-    start_m = time.time()
-    matrix_circuit(qubit)
-    end_m = time.time()
-    matrix_runtimes.append(end_m-start_m)
+    if qubit<15:
+        start_m = time.time()
+        matrix_circuit(qubit)
+        end_m = time.time()
+        matrix_runtimes.append(end_m-start_m)
 
     start_t = time.time()
     tensor_circuit(qubit)
     end_t = time.time()
     tensor_runtimes.append(end_t-start_t)
+np.savetxt('matrix_runtimes.txt', matrix_runtimes)
+np.savetxt('tensor_runtimes.txt', tensor_runtimes)
+matrix_data = np.loadtxt('matrix_runtimes.txt')
+tensor_data = np.loadtxt('tensor_runtimes.txt')
+fig, ax = plt.subplots(1, 2, figsize=(10, 4))  # 1 row, 2 columns
 
-plt.plot(x, matrix_runtimes, label = 'state vector')
-plt.plot(x, tensor_runtimes, label = 'tensor')
-plt.xlabel('no of qubits')
-plt.ylabel('runtime (s)')
-plt.legend()
+# First plot (sin curve)
+ax[0].plot(x[0:15], matrix_data, color='b')
+ax[0].set_title('State vector')
+ax[0].set_xlabel('qubit no')
+ax[0].set_ylabel('runtime')
+
+# Second plot (cos curve)
+ax[1].plot(x, tensor_data, color='r')
+ax[1].set_title('Tensor')
+ax[1].set_xlabel('qubit no')
+ax[1].set_ylabel('runtime')
+
+# Adjust the layout
+plt.tight_layout()
+
+# Show the plot
 plt.show()
 
