e378bb892ed9e7e63fef583e2245f5d27a1e619a
Matthew Kendall <47122515+mattkendall11@users.noreply.github.com>
2024-10-16 08:25:39 +0100

    nearly done

--- task_one.py
+++ task_one.py
@@ -10,21 +10,21 @@ from utils.tensor import tensor_circuit
 script to compare the runtime of state-vector and tensor simulations of quantum circuits for the exact same circuit
 '''
 x = list(range(2,25))
-# matrix_runtimes = []
-# tensor_runtimes = []
-# for qubit in tqdm(x):
-#     if qubit<15:
-#         start_m = time.time()
-#         matrix_circuit(qubit)
-#         end_m = time.time()
-#         matrix_runtimes.append(end_m-start_m)
-#
-#     start_t = time.time()
-#     tensor_circuit(qubit)
-#     end_t = time.time()
-#     tensor_runtimes.append(end_t-start_t)
-# np.savetxt('matrix_runtimes.txt', matrix_runtimes)
-# np.savetxt('tensor_runtimes.txt', tensor_runtimes)
+matrix_runtimes = []
+tensor_runtimes = []
+for qubit in tqdm(x):
+    if qubit<15:
+        start_m = time.time()
+        matrix_circuit(qubit)
+        end_m = time.time()
+        matrix_runtimes.append(end_m-start_m)
+
+    start_t = time.time()
+    tensor_circuit(qubit)
+    end_t = time.time()
+    tensor_runtimes.append(end_t-start_t)
+np.savetxt('matrix_runtimes.txt', matrix_runtimes)
+np.savetxt('tensor_runtimes.txt', tensor_runtimes)
 matrix_data = np.loadtxt('matrix_runtimes.txt')
 tensor_data = np.loadtxt('tensor_runtimes.txt')
 fig, ax = plt.subplots(1, 2, figsize=(10, 4))  # 1 row, 2 columns
